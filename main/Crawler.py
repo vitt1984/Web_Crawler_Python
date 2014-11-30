a6c0a77b07fd70bb237362f6d72765003238f6a3
vitt1984 <vittorio.turri@gmail.com>
2014-11-30 18:50:58 +0100

    Added argparse

--- main/Crawler.py
+++ main/Crawler.py
@@ -15,9 +15,22 @@ from urllib.parse import urlparse
 import pprint
 import operator
 import math
-            
+import argparse
+
 if __name__ == "__main__":
     
+    parser = argparse.ArgumentParser(description='Web Crawlers: return 10 most relevant pages and within these, the most relevant image.')
+    parser.add_argument('-s', dest='startPage', action='store', required=True,
+                       help='the first page that will be checked')
+    parser.add_argument('-w', dest='words', nargs='+', required=True,
+                       help='words to look for')
+    parser.add_argument('--maxDepth', dest='maxDepth', type=int, default=1, required=False,
+                       help='depth of link search. Default value is 1')
+    parser.add_argument('--nbSpiders', dest='nbSpiders', type=int, default=3, required=False,
+                       help='number of spiders that will be generated. Default value is 3')
+    
+    args = parser.parse_args()
+
     pp = pprint.PrettyPrinter(indent=4)
         
     m = multiprocessing.Manager()
@@ -25,23 +38,25 @@ if __name__ == "__main__":
     visitedPages = m.dict()
     pagesWithImage = m.dict()
     processes = []
-
-    startPage="http://www.ilfattoquotidiano.it/"
-    words = ['renzi']
+    
+    print ("Starting website: ", args.startPage)
+    print ("Looking for words: ", args.words)
+    print ("Max depth: ", args.maxDepth)
+    print ("Number of spiders: ", args.nbSpiders)
     
     print ("Assigning weight to words...")
     
-    pagesQueue.put([startPage,0])
+    pagesQueue.put([args.startPage,0])
     
     weightedWords = defaultdict(int)
-    for word in words:
-        weightedWords[word.lower()] = math.pow(len(words)-words.index(word),2)
+    for word in args.words:
+        weightedWords[word.lower()] = math.pow(len(args.words)-args.words.index(word),2)
     
     pp.pprint(sorted(weightedWords.items(), key=operator.itemgetter(1)))
     
     print ("Creating spiders...")
     
-    aPageSpider = PageSpider.PageSpider(weightedWords, 1, "http://" + urlparse(startPage).hostname)
+    aPageSpider = PageSpider.PageSpider(weightedWords, args.maxDepth, "http://" + urlparse(args.startPage).hostname)
     aImageSpider = ImageSpider.ImageSpider(weightedWords)
     
     print ("Sending spiders...")
@@ -49,10 +64,11 @@ if __name__ == "__main__":
     p1 = Process(target=aPageSpider.run, args=[pagesQueue,visitedPages])
     processes.append(p1)
     p1.start()
-        
+    
+    # giving some time for the spider to fetch the first page and put some links inthe queue
     time.sleep(10)
     
-    for i in range(3):
+    for i in range(args.nbSpiders):
         p = Process(target=aPageSpider.run, args=[pagesQueue,visitedPages])
         processes.append(p)
         p.start()
@@ -68,7 +84,7 @@ if __name__ == "__main__":
         if visitedPages[visitedPage] == 0:
             del visitedPages[visitedPage]
     
-    print ("Ordering dict...")
+    print ("Ordering visited pages...")
     
     tenMostMatchingPages = sorted(visitedPages.items(), key=operator.itemgetter(1))[-10:]
     
@@ -80,6 +96,8 @@ if __name__ == "__main__":
         pagesQueue.put(pageWithWeight[0])
 
     aImageSpider.run(pagesQueue,pagesWithImage)
+    
+    print ("Most relevant images:")
 
     pp.pprint(dict(pagesWithImage))
 
